48e9499c6ad57d2e50b89630a5e43d194676bfe7
Anika Mayer <mayer.anika@protonmail.com>
2026-07-08 17:14:23 +0200

    add: dialog

--- script.js
+++ script.js
@@ -47,7 +47,9 @@ const photoArray = [
         name: "Woman watching cityscape",
         path: "woman_view.jpg",
     }
-]
+];
+
+// rendering photo-boxes
 
 function renderGallery() {
     const photo_containerRef = document.getElementById('photo_container');
@@ -55,9 +57,61 @@ function renderGallery() {
     for (let i = 0; i < photoArray.length; i++) {
         photo_containerRef.innerHTML += /*html*/`
         <div class="photo_box">
-            <img src="../assets/img/${photoArray[i].path}" alt="${photoArray[i].name}">
+            <img id="photo$(photoArray[i])" onclick="openDialog(${i})" src="../assets/img/${photoArray[i].path}" alt="${photoArray[i].name}">
         </div>
     `
         
     }
+}
+
+// render dialog
+
+const photo_dialogRef = document.getElementById('photo_dialog');
+
+function templateDialog(i) {
+    return /*html*/`
+    <div class="dialog_box" onclick="BubblingProtection(event)">
+        <div class="dialog_head">
+            <h2>${photoArray[i].name}</h2>
+            <button onclick="closeDialog()"><img src="./assets/icons/close_button.svg" alt="closing_button"></button>
+        </div>
+        <div class="dialog_img">
+            <img src="./assets/img/${photoArray[i].path}" alt="${photoArray[i].name}">
+        </div>
+        <div class="dialog_foot">
+            <button class="button_left" onclick="changeToPrevPhoto(${i})"><img src="./assets/icons/arrow.svg" alt=""></button>
+            <p>${[i + 1]}/${photoArray.length}</p>
+            <button class="button_right" onclick="changeToNextPhoto(${i})"><img src="./assets/icons/arrow.svg" alt="next_button"></button>
+        </div>
+    </div>
+    `
+}
+
+function openDialog(i) {
+    photo_dialogRef.showModal();
+    photo_dialogRef.innerHTML = templateDialog(i);
+}
+
+function changeToNextPhoto(i) {
+    photo_dialogRef.innerHTML = "";
+
+    if(i>=0 && i<photoArray.length - 1) {i++} 
+    else {i=0};
+    photo_dialogRef.innerHTML = templateDialog(i);
+}
+
+function changeToPrevPhoto(i) {
+    photo_dialogRef.innerHTML = "";
+
+    if(i>0) {i--}
+    else {i=photoArray.length - 1};
+    photo_dialogRef.innerHTML = templateDialog(i);
+}
+
+function closeDialog() {
+    photo_dialogRef.close();
+}
+
+function BubblingProtection(event) {
+    event.stopPropagation();
 }
